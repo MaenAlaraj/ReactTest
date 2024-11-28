993f28cc70743b5fbc409f30af2617b72fc5eccc
maen.alaraj <maen.alaraj@gck.co.jp>
2024-11-28 14:14:39 +0900

    Initial commit

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -7,7 +7,7 @@ const GlobalContext = createContext();
 export const GlobalProvider = ({ children }) => {
   const [showSecondScreenFlg, setShowSecondScreenFlg] = useState(true);
   const [gckid, setGckid] = useState("");
-  const [prefix, setPrefix] = useState("");
+  const [prefix, setPrefix] = useState("93929");
   const [sbuser, setSbuser] = useState("93929" + "09000000013");
   const [extractedUserID, setExtractedUserID] = useState("");
   const [userBeforePrefix, setUserBeforePrefix] = useState("");
