13c46b97f13e71ac520690fb18a2d8b9249b472c
maen.alaraj <maen.alaraj@gck.co.jp>
2024-11-29 11:33:54 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -10,19 +10,53 @@ import Payment from './components/PaymentButton';
 const App = () => {
   return (
     <div className="App">
-      <div className="auth-container">
-        <p>GC Mall アプリ</p>
-        <p>GCのQRか顔を読み込ませてください</p>
+      {/* Authentication Container */}
+      <div className="auth-container" id="authContainer">
+        <p>GC Mall</p>
+        <p>顔またはGCのQRを読み込ませてください</p>
         <div className="button-container">
           <FaceButton />
           <QRButton />
         </div>
-        <RegisterFaceButton />
+        <div id="bottomLeftText">
+          <span className="text-content"></span>
+        </div>
+        <RegisterFaceButton /> {/* Register Face Button */}
       </div>
-      <div className="scroll-container" id="productList">
-        <LoadProduct />
+
+      {/* Transit Container */}
+      <div className="auth-container" id="transitContainer" style={{ display: "none" }}>
+        <p>処理中...</p>
       </div>
-      <Payment />
+
+      {/* Main Content After Successful Authentication */}
+      <div id="mainContainer" style={{ display: "none" }}>
+        <div id="user-container">
+          <p id="user"></p>
+          <p id="wifiStatus">Wi-Fiの状態: 確認中...</p> {/* Wi-Fi Status */}
+          <button id="logoutButton">ログアウト</button>
+        </div>
+        <p id="message"></p>
+        <button id="loadProduct">商品の読み込み</button>
+
+        <div className="scroll-container">
+          <ul id="productList">
+            <li className="header-item">
+              <span>コード</span>
+              <span>商品名</span>
+              <span>ポイント</span>
+              <span>CAT.</span>
+              <span>日付</span>
+              <span>削除</span>
+            </li>
+          </ul>
+        </div>
+        <p id="Balance"></p>
+        <div id="totalAmount">トータル: 0pt</div>
+        <div className=".vertical">
+          <Payment />
+        </div>
+      </div>
     </div>
   );
 };
