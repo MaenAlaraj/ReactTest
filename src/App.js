6f17c0e971252624ad1aca3d5f012540ff3a3013
maen.alaraj <maen.alaraj@gck.co.jp>
2024-12-05 13:24:39 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import LogoutButton from './components/LogoutButton';
 import WiFiStatus from './components/WiFiStatus';
 
 
+
 const App = () => {
   const { balanceMessage } = useGlobalContext();
 
