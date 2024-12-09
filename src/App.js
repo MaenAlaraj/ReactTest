84eaa1c2e9a86cacc31751f9ed7835cf7c79edcd
maen.alaraj <maen.alaraj@gck.co.jp>
2024-12-09 09:37:06 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-// src/App.js
-
 import React, { useState } from "react";
-import './style.css';
-import './App.css'; // Import the CSS file here
+import "./style.css";
+import "./App.css";
 import { useGlobalContext } from "./GlobalContext";
 
-import RegisterFaceButton from './components/RegisterFaceButton';
-import FaceButton from './components/FaceButton';
-import QRButton from './components/QrButton';
-import LoadProductButton  from './components/LoadProductButton';
-import Payment from './components/PaymentButton';
-import LogoutButton from './components/LogoutButton';
-import WiFiStatus from './components/WiFiStatus';
-
-
+import RegisterFaceButton from "./components/RegisterFaceButton";
+import FaceButton from "./components/FaceButton";
+import QRButton from "./components/QrButton";
+import LoadProductButton from "./components/LoadProductButton";
+import Payment from "./components/PaymentButton";
+import LogoutButton from "./components/LogoutButton";
+import WiFiStatus from "./components/WiFiStatus";
 
 const App = () => {
   const { balanceMessage } = useGlobalContext();
@@ -42,26 +38,17 @@ const App = () => {
         <p>処理中...</p>
       </div>
 
-
-
-
-
-
-
       {/* Main Content After Successful Authentication */}
       <div id="mainContainer" style={{ display: "none" }}>
         <div id="user-container">
           <p id="user"></p> {/* User information */}
           <div className="user-details-container">
-            <WiFiStatus /> {/* WiFi status aligned with user */}
-            <LogoutButton id="logoutButton" /> {/* Logout button below WiFi status */}
+            <WiFiStatus />
+            <LogoutButton id="logoutButton" />
           </div>
         </div>
         <p id="message">{balanceMessage}</p> {/* Balance message below LogoutButton */}
 
-
-
-        
         {/* Button to show LoadProductButton */}
         {!showLoadProductButton && (
           <button id="loadProduct" onClick={() => setShowLoadProductButton(true)}>
@@ -70,9 +57,14 @@ const App = () => {
         )}
 
         {/* Render the LoadProductButton component */}
-        {showLoadProductButton && <LoadProductButton />}
-
-        
+        {showLoadProductButton && (
+          <LoadProductButton
+            productList={productList}
+            setProductList={setProductList}
+            totalAmount={totalAmount}
+            setTotalAmount={setTotalAmount}
+          />
+        )}
 
         <div className="scroll-container">
           <ul id="productList">
@@ -84,10 +76,29 @@ const App = () => {
               <span>日付</span>
               <span>削除</span>
             </li>
+            {productList.map((item) => (
+              <li key={item.index}>
+                <span>{item.seller}</span>
+                <span>{item.product}</span>
+                <span>{item.price} pt</span>
+                <span>{item.category}</span>
+                <span>{item.date}</span>
+                <div
+                  className="remove-item"
+                  onClick={() => {
+                    const updatedList = productList.filter((i) => i.index !== item.index);
+                    setProductList(updatedList);
+                    setTotalAmount((prevTotal) => prevTotal - item.price);
+                  }}
+                >
+                  ❌
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
         <p id="Balance"></p>
-        <div id="totalAmount">トータル: 0pt</div>
+        <div id="totalAmount">トータル: {totalAmount} pt</div>
         <div className=".vertical">
           <Payment />
         </div>
