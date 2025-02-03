16249889f72d25eb11a32aa5065058d42f5462ac
maen.alaraj <maen.alaraj@gck.co.jp>
2025-02-03 18:00:15 +0900

    Initial commit

--- src/components/FaceButton.jsx
+++ src/components/FaceButton.jsx
@@ -15,8 +15,8 @@ const FaceButton = () => {
   const handleFace = async () => {
     console.log("[FaceButton]:「顔認証」ボタンがクリックされました。");
 
-    //var info = await window.CCWalletInterface.DelFaces(sbuser,"9392909000000154"); //アララジ    
-    //console.log("[Delete Button]:Responce of CCWalletInterface.DelFaces:", info);
+    var info = await window.CCWalletInterface.DelFaces(sbuser,"9392909000000154"); //アララジ    
+    console.log("[Delete Button]:Responce of CCWalletInterface.DelFaces:", info);
 
     // Show the transitContainer and hide authContainer
     /*document.getElementById("authContainer").style.display = "none";
