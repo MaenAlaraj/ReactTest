a997b67938c25e31e6e7a8d92b32a3b5c44fe4bb
maen.alaraj <maen.alaraj@gck.co.jp>
2025-02-03 17:53:09 +0900

    Initial commit

--- src/components/FaceButton.jsx
+++ src/components/FaceButton.jsx
@@ -19,7 +19,7 @@ const FaceButton = () => {
     //console.log("[Delete Button]:Responce of CCWalletInterface.DelFaces:", info);
 
     // Show the transitContainer and hide authContainer
-    document.getElementById("authContainer").style.display = "none";
+    /*document.getElementById("authContainer").style.display = "none";
     document.getElementById("transitContainer").style.display = "block";
 
     try {
@@ -79,7 +79,7 @@ const FaceButton = () => {
       setMessage(`[FaceButton]エラーが発生しました: ${error.message}`, "show_message");
       document.getElementById("transitContainer").style.display = "none";
       document.getElementById("authContainer").style.display = "block";
-    }
+    }*/
   };
 
   return (
