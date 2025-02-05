a02a37eba332f3509009de3dd8d2b9fe93ad5838
maen.alaraj <maen.alaraj@gck.co.jp>
2025-02-05 15:50:09 +0900

    Initial commit

--- src/components/FaceButton.jsx
+++ src/components/FaceButton.jsx
@@ -15,14 +15,14 @@ const FaceButton = () => {
   const handleFace = async () => {
     console.log("[FaceButton]:「顔認証」ボタンがクリックされました。");
 
-    //var info = await window.CCWalletInterface.DelFaces(sbuser,"9392909000000154"); //アララジ    
-    //console.log("[Delete Button]:Responce of CCWalletInterface.DelFaces:", info);
+    var info = await window.CCWalletInterface.DelFaces(sbuser,"9392909000000154"); //アララジ    
+    console.log("[Delete Button]:Responce of CCWalletInterface.DelFaces:", info);
 
     //var info = await window.CCWalletInterface.DelFaces(sbuser,"9392909000000219"); //伊藤さん    
     //console.log("[Delete Button]:Responce of CCWalletInterface.DelFaces:", info);
 
     // Show the transitContainer and hide authContainer
-    document.getElementById("authContainer").style.display = "none";
+    /*document.getElementById("authContainer").style.display = "none";
     document.getElementById("transitContainer").style.display = "block";
 
     try {
@@ -82,7 +82,7 @@ const FaceButton = () => {
       setMessage(`[FaceButton]エラーが発生しました: ${error.message}`, "show_message");
       document.getElementById("transitContainer").style.display = "none";
       document.getElementById("authContainer").style.display = "block";
-    }
+    }*/
   };
 
   return (
